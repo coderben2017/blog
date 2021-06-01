6e810f210d1d2c6ac15c82592d8871189c44420b
coderben2017 <coderben2016@126.com>
2021-06-01 17:53:41 +0800

    规范返回体结构

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ const ip = getLocalIP()
 const port = 3001
 
 // 数据源，简单模拟一个数据库
+const users = [
+  {userName: 'admin', password: 'admin'}
+]
 let total = 789
 let articles = []
 for (let i = 0; i < total; i++) {
@@ -36,33 +39,80 @@ app.all('*', function (req, res, next) {
 });
 
 
+app.post('/login', (req, res) => {
+  const {userName, password} = req.body
+  if (users.find(user => user.userName === userName && user.password === password)) {
+    res.json({
+      code: 1,
+      data: {userName},
+      msg: 'success'
+    })
+  } else {
+    res.json({
+      code: 0,
+      data: null,
+      msg: '账号密码错误'
+    })
+  }
+})
+
 app.get('/articles', (req, res) => {
-  let { start, limit } = req.query
-  start = Number(start) || 0
-  limit = Number(limit) || (articles.length - 1)
-  const data = articles.slice(start, start + limit)
-  res.json({
-    data,
-    total
-  })
+  try {
+    let { start, limit } = req.query
+    start = Number(start) || 0
+    limit = Number(limit) || (articles.length - 1)
+    const data = articles.slice(start, start + limit)
+    res.json({
+      code: 1,
+      data,
+      total,
+      msg: 'success'
+    })
+  } catch (e) {
+    res.json({
+      code: 0,
+      data: null,
+      msg: JSON.stringify(e)
+    })
+  }
 })
 
 app.get('/article', (req, res) => {
   const { id } = req.query
   const data = articles.find(article => article.id === id)
-  res.json({
-    data,
-  })
+  if (data) {
+    res.json({
+      code: 1,
+      data,
+      msg: 'success'
+    })
+  } else {
+    res.json({
+      code: 0,
+      data: null,
+      msg: '未找到该文章'
+    })
+  }
 })
 
 app.post('/article', (req, res) => {
-  let { id, title, content, author, createTime } = req.body
-  if (!id) id = +new Date() + Math.random() + ''
-  articles.unshift({id, title, content, author, createTime})
-  ++total
-  res.json({
-    data: {id, title, content, author, createTime}
-  })
+  try {
+    let { id, title, content, author, createTime } = req.body
+    if (!id) id = +new Date() + Math.random() + ''
+    articles.unshift({id, title, content, author, createTime})
+    ++total
+    res.json({
+      code: 1,
+      data: {id, title, content, author, createTime},
+      msg: 'success'
+    })
+  } catch (e) {
+    res.json({
+      code: 0,
+      data: null,
+      msg: JSON.stringify(e)
+    })
+  }
 })
 
 
